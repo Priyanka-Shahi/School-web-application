52b8a339b73c9013f382bed39d853d40b4e5828c
Priyanka-Shahi <shahipriyan@gmail.com>
2022-11-03 19:03:02 +1100

    student index action and view done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'students/index'
-  get 'students/show'
-
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "houses#index"
   
-  resources "houses", only: %i[index show]
+  resources "houses", only: [:index, :show]
+
+  resources "students", only: [:index, :show]
 end
