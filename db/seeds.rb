c4b15183c9a8ac2b99a4142bcb756dcae6c45e64
Priyanka-Shahi <shahipriyan@gmail.com>
2022-11-03 12:41:19 +1100

    added db for student and houses in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,13 @@ NUMBER_OF_HOUSES = 5
 NUMBER_OF_HOUSES.times do
     house_name = Faker::Movies::HarryPotter.unique.house
     house_points = rand(333..9999)
-    House.create(name: house_name , points: house_points) 
+    house = House.create(name: house_name , points: house_points)
+    
+    number_of_students = rand( 10..15)
+    number_of_students.times do
+        house.students.create(name: Faker::Movies::HarryPotter.unique.character)
+    end
 end
 
 puts "Created #{House.count} Houses."
+puts "Created #{Student.count} Student."
