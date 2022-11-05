728deec8025c2af37ae11296f54e1cd430b66355
Priyanka-Shahi <shahipriyan@gmail.com>
2022-11-05 18:26:00 +1100

    added search option

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,11 @@
+class SearchController < ApplicationController
+
+    def index
+    end
+    
+  
+    def results
+      @students = Student.where('name LIKE ?', "%#{params[:q]}%")
+    end
+   
+  end
