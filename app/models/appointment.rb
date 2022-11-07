4f476aaaf9c3551bab080300fc617adeadfaabc7
Priyanka-Shahi <shahipriyan@gmail.com>
2022-11-07 23:11:45 +1100

    added validation for appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,5 +3,12 @@ class Appointment < ApplicationRecord
   belongs_to :student
   
   validates :appointment_date, presence: true
-  
+  validate :appointment_date_cannot_be_in_the_past
+
+  def appointment_date_cannot_be_in_the_past
+    if appointment_date.present? && appointment_date < Date.today
+      errors.add(:appointment_date, "can't be in the past")
+    end
+  end
 end
+
